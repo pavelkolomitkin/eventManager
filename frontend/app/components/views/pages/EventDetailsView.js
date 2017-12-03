b9ce8b731dfff4140e9e185a96bb9a302c98dd42
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2017-12-03 15:23:08 +0300

    add delete function

--- frontend/app/components/views/pages/EventDetailsView.js
+++ frontend/app/components/views/pages/EventDetailsView.js
@@ -1,8 +1,32 @@
 import ContentView from './ContentView';
 import Event from '../../models/Event';
 import template from '../../../templates/pages/eventDetails.jst';
+import AppRouter from '../../AppRouter';
 
 export default ContentView.extend({
     model: Event,
-    template: template
+    template: template,
+    triggers: {
+        'click #delete-button': 'delete:event'
+    },
+
+    onDeleteEvent()
+    {
+        if (confirm('Вы действительно хотите удалить событие?'))
+        {
+            this.model.destroy({
+                success: function(model, response){
+                    const router = AppRouter.getInstance();
+                    router.navigateWithTrigger(router.generatePath(AppRouter.Routes.EVENTS));
+                },
+                error: function(model, response){
+                    alert('Ошибка удаления события!');
+
+                    const router = AppRouter.getInstance();
+                    router.navigateWithTrigger(router.generatePath(AppRouter.Routes.EVENTS));
+                }
+            })
+        }
+    }
+
 });
